f88320ec4ea53762889033985f1d214eae65ae4a
0xc0d3r <xc0d3r.r3b00t@gmail.com>
2015-07-15 17:06:52 +0530

    Layered packet details displayed

--- scapy/test.py
+++ scapy/test.py
@@ -1,7 +1,18 @@
 from scapy.all import *
 
 tcp = TCP()
-
-print "[+] Displaying TCP details..."
+print "[+] Displaying TCP Packet details..."
 print tcp.show()
 
+ip = IP()
+print "[+] Displaying IP Packet details..."
+print ip.show()
+
+ether = Ether()
+print "[+] Displaying Ether details..."
+print ether.show()
+
+print "[+] Layering packets..."
+packet = ether/ip/tcp
+print "[+] Displaying layered packet details..."
+packet.show()
